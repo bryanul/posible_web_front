24324f1753f273087b5de7a58f47efc734f5eff6
Bryan Ugas <38958582+bryanul@users.noreply.github.com>
2024-03-25 21:53:59 -0500

    xd?

--- src/index.js
+++ src/index.js
@@ -6,16 +6,13 @@ import {BrowserRouter} from 'react-router-dom';
 import { HashRouter } from 'react-router-dom';
 import {NextUIProvider} from "@nextui-org/react";
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <NextUIProvider>
-            <HashRouter>
-            {/*<BrowserRouter>*/}
+            <BrowserRouter>
                 <App/>
-            {/*</BrowserRouter>*/}
-            </HashRouter>
+            </BrowserRouter>
         </NextUIProvider>
     </React.StrictMode>
 );

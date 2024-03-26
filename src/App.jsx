35badc893cc1e5ef4214d45fec7fdb97213a70ec
Bryan Ugas <38958582+bryanul@users.noreply.github.com>
2024-03-25 21:12:26 -0500

    pages4

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
         <>
             <Routes>
                 <Route
-                    path="*"
+                    path=""
                     element={<Navigate to="/main" replace/>}
                 />
                 <Route path="/main" element={<MainPageESP />} />
